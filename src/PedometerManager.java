5ec0130ead74907917324c445b3cc894d5a00278
yeonseungmin <tmdals4544@gnu.ac.kr>
2022-03-11 23:48:05 +0900

    removed hello.java and added pedometermanager.java

---
+++ src/PedometerManager.java
@@ -0,0 +1,14 @@
+
+public class PedometerManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. Add Students");
+		System.out.println("2. Delete Students");
+		System.out.println("3. Edit Students");
+		System.out.println("4. View Students");
+		System.out.println("5. Exit");
+		
+	}
+
+}
